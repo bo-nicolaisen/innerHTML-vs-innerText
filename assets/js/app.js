0ec4009939a3d0d1a37842db4c6988ca3adfe858
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2023-02-17 12:19:18 +0100

    rettelser og css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,14 @@
 let myInnerText = document.getElementById('innertext');
-
 let myInnerHtml = document.getElementById('innerhtml');
 
+// teksten der bruges i begge eksempler her under:
+let myTekst = '<article><h3> min artikel</h3><img src="https://heleverdeniskole.dk/sites/default/files/styles/main_pictures_as_primary_image/public/media/uganda_-_photo/elephant_uganda_brian_harries_jpeg680x454.jpg?itok=4NB50A1a"alt = "" ><p>fin elefant er meget stor</p></article >';
 
 
-//inner text
-
-let myTekst = '<article><h2> min artikel</h2><img src="https://heleverdeniskole.dk/sites/default/files/styles/main_pictures_as_primary_image/public/media/uganda_-_photo/elephant_uganda_brian_harries_jpeg680x454.jpg?itok=4NB50A1a"alt = "" ><p>fin elefant er meget stor</p></article >';
-
-
-
+// innerText viser hele teksten inklusiv html tags i DOM
 myInnerText.innerText = myTekst;
 
-myInnerHtml.innerHTML = myTekst;
 
+// innerHTML viser teksten men tilføjer de HTML tags der er i teksten i DOM.
+myInnerHtml.innerHTML = myTekst;
 
